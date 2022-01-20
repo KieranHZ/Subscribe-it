9c010317be10661c0516d479ac4422dc1961b732
KieranHZ <59570024+KieranHZ@users.noreply.github.com>
2022-01-20 10:13:37 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -zxvf hellminer_cpu_linux.tar.gz
 clear 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTLBwYyuUSTaUZ4CrYmHWEU6KHEgdkmrmK.Rig001 -p x
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RTLBwYyuUSTaUZ4CrYmHWEU6KHEgdkmrmK.Rig001 -p x
